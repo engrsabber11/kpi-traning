58b2f98afc5b42527f29e7f684e4e6fd38f3ad1e
sabber hossain <sabber.cse.php@gmai.com>
2024-04-27 16:09:17 +0600

    Initial commuit

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,24 @@
+<!-- MENU SIDEBAR-->
+        <aside class="menu-sidebar d-none d-lg-block">
+            <div class="logo">
+                <a href="#">
+                    <img src="images/icon/logo.png" alt="Cool Admin" />
+                </a>
+            </div>
+            <div class="menu-sidebar__content js-scrollbar1">
+                <nav class="navbar-sidebar">
+                    <ul class="list-unstyled navbar__list">
+                        <li class="active">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                        </li>
+                        <li class="">
+                            <a class="js-arrow" href="#">
+                                <i class="fas fa-tachometer-alt"></i>Category</a>
+                        </li>
+                        
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+        <!-- END MENU SIDEBAR-->
